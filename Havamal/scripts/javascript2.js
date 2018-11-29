8a8714e6207bd37faa36712b8d8dfb6d2aa9ed84
lolaphoenix <32101508+lolaphoenix@users.noreply.github.com>
2018-11-29 16:46:41 +0000

    Update javascript2.js

--- Havamal/scripts/javascript2.js
+++ Havamal/scripts/javascript2.js
@@ -1,7 +1,7 @@
 var quotes = [
 'TEST ONE',
 'TEST TWO',
-  ‘164 Now the words of the One-Eyed are heard in Valhalla, for the benefit of humans, for the harm of giants, health to you who speak them, health to you who know them, joy to you who learn them, health to you who hear them.’
+  'Test three',
 ]
 
 function newQuote() {
